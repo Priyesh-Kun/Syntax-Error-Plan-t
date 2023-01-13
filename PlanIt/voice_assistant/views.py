25f7292f31838c15747a5a3b6b48406b19d47c7a
Priyesh-Kun <mishrapriyesh2004@gmail.com>
2023-01-14 02:54:26 +0530

    added the voice to text function

--- PlanIt/voice_assistant/views.py
+++ PlanIt/voice_assistant/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import speech_recognition as sr
+from os import path 
 
 # Create your views here.
+def voice_to_text():
+    audio_file = path.join(path.dirname(path.realpath('Recording.wav')),'Recording.wav')
+    recog = sr.Recognizer()
+    with sr.AudioFile(audio_file) as source:
+        input = recog.record(source)
+    try:
+        text = recog.recognize_google(input,language='en-in')
+    except Exception:
+        print("Error")
+        return 0
+    return text
