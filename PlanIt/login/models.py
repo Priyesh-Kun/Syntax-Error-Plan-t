b810112dcb56899a5a8b1324314e02ece15c14e0
Priyesh-Kun <mishrapriyesh2004@gmail.com>
2023-01-14 04:20:57 +0530

    Aise hi kardiya

--- PlanIt/login/models.py
+++ PlanIt/login/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    user_name=models.CharField(max_length=50)
+    name=models.CharField(max_lenght=122)
+    email=models.CharField(max_length=122)
+    pass_word = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
--- PlanIt/login/views.py
+++ PlanIt/login/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from login.models import Users
 # Create your views here.
+def sign_up(request):
+    if request.method == 'POST':
+        user_name = request.POST.get('user_name')
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        pass_word = request.POST.get('pass_word')
+        user = Users(user_name=user_name,name=name,email=email,pass_word=pass_word)
+        user.save()
+        return render(request, 'index.html')
+    return render(request, 'signup.html')
+
+def login(request):
+    username = request.POST.get('user_name')
+    password = request.POST.get('pass_work')
+    form = Users.objects.all()
+    context = {'form' : form}
+    for data in form:
+        if data.user_name == username:
+            if data.pass_word == password:
+                return render(request, 'index.html')
+            else:
+                return render(request, 'login.html')
+        else:
+            return render(request,'login.html') 
