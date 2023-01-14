1d24e47b68b875702989dd19fa27c001145310bb
Priyesh-Kun <mishrapriyesh2004@gmail.com>
2023-01-14 14:21:48 +0530

    added extra classes of daily tasks extra

--- PlanIt/login/models.py
+++ PlanIt/login/models.py
@@ -3,8 +3,27 @@ from django.db import models
 # Create your models here.
 class Users(models.Model):
     user_name=models.CharField(max_length=50)
-    name=models.CharField(max_lenght=122)
+    name=models.CharField(max_length=122)
     email=models.CharField(max_length=122)
     pass_word = models.CharField(max_length=20)
+    pass_word_confirm = models.CharField(max_length=20)
     def __str__(self):
         return self.name
+
+class DailyTasks(models.Model):
+    user_name=models.CharField(max_length=50)
+    daily_tasks=models.CharField(max_length=120)
+    def __str__(self):
+        return self.user_name
+
+class WeeklyTasks(models.Model):
+    user_name=models.CharField(max_length=50)
+    Weekly_tasks=models.CharField(max_length=120)
+    def __str__(self):
+        return self.user_name
+
+class MonthlyTasks(models.Model):
+    user_name=models.CharField(max_length=50)
+    Monthly_tasks=models.CharField(max_length=120)
+    def __str__(self):
+        return self.user_name
