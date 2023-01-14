546fded218ec9e312a3d163ec24f453d92c29576
Priyesh-Kun <mishrapriyesh2004@gmail.com>
2023-01-14 15:15:59 +0530

    Added the completion field in the models

--- PlanIt/login/models.py
+++ PlanIt/login/models.py
@@ -13,17 +13,20 @@ class Users(models.Model):
 class DailyTasks(models.Model):
     user_name=models.CharField(max_length=50)
     daily_tasks=models.CharField(max_length=120)
+    completion = models.BooleanField(default=0)
     def __str__(self):
         return self.user_name
 
 class WeeklyTasks(models.Model):
     user_name=models.CharField(max_length=50)
     Weekly_tasks=models.CharField(max_length=120)
+    completion = models.BooleanField(default=0)
     def __str__(self):
         return self.user_name
 
 class MonthlyTasks(models.Model):
     user_name=models.CharField(max_length=50)
     Monthly_tasks=models.CharField(max_length=120)
+    completion = models.BooleanField(default=0)
     def __str__(self):
         return self.user_name
