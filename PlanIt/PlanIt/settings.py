49a6ca375734a881c08229a4b3352fbe6a71290e
Yukta Agrawal <yuktaagarwal8@gmail.com>
2023-01-15 04:31:25 +0530

    adding the calculator app in the settings

--- PlanIt/PlanIt/settings.py
+++ PlanIt/PlanIt/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'todo.apps.TodoConfig',
+    'calculator.apps.CalculatorConfig',
     'login.apps.LoginConfig',
     'django.contrib.admin',
     'django.contrib.auth',
