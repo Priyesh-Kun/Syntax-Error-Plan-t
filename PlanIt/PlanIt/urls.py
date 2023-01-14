5c3239a120185b539411de07afb5ef12c0193b18
Priyesh-Kun <mishrapriyesh2004@gmail.com>
2023-01-14 14:29:09 +0530

    included login urls

--- PlanIt/PlanIt/urls.py
+++ PlanIt/PlanIt/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from login import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('login.urls')),
+    path('signup',views.sign_up,name='signup')
 ]
